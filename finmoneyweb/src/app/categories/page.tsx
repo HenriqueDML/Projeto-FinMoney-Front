7bf750b07fc328e2d22331e6d3ad43a22c239970
HenriqueDML <164499545+HenriqueDML@users.noreply.github.com>
2025-03-20 10:45:12 -0300

    Update page.tsx

--- finmoneyweb/src/app/categories/page.tsx
+++ finmoneyweb/src/app/categories/page.tsx
@@ -1,11 +1,11 @@
 export default function CategoriesPage() {
     return (
         <nav>
-            <h1></h1>
+            <h1>FinMoney</h1>
             <ul>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
+                <li><a href="#">dashboard</a></li>
+                <li><a href="#">movimentações</a></li>
+                <li><a href="#">caregories</a></li>
             </ul>
         </nav>
     )
