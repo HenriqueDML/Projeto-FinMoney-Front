568aa215aad62bac06b3a1f6fad7d51a90a2810f
HenriqueDML <164499545+HenriqueDML@users.noreply.github.com>
2025-03-20 10:43:56 -0300

    criando categories

---
+++ finmoneyweb/src/app/categories/page.tsx
@@ -0,0 +1,12 @@
+export default function CategoriesPage() {
+    return (
+        <nav>
+            <h1></h1>
+            <ul>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </nav>
+    )
+}
