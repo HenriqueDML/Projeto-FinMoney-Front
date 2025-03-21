229eb53d3e8e4062fa1da870d14c888861f13f65
Henrique <henrique.garcia.cap698@gmail.com>
2025-03-21 10:32:13 -0300

    CaregotyItems e Lucide

--- finmoneyweb/src/app/categories/page.tsx
+++ finmoneyweb/src/app/categories/page.tsx
@@ -1,3 +1,4 @@
+import CategotyItem from "@/components/category-item";
 import NavBar from "@/components/nav-bar"; 
 
 export default function CategoriesPage() {
@@ -8,6 +9,7 @@ export default function CategoriesPage() {
             <main className="flex items-center justify-center">
                 <div className="bg-slate-900 rounded p-6 m-6 min-w-2/3">
                     <h2 className="text-lg font-bold">Categorias</h2>
+                    <CategotyItem/>
                 </div>
             </main>
         </>
---
+++ finmoneyweb/src/components/category-item.tsx
@@ -0,0 +1,13 @@
+export default function CategotyItem() {
+  return (
+    <div>
+      <div className="flex justify-between">
+        <span>icone</span>
+        <span>nome</span>
+      </div>
+      <div>
+        <span>dropdown</span>
+      </div>
+    </div>
+  );
+}
